ff2c261aeb528c552b391eddc77fab2e7d72f209
matheussOliveira1 <matheus.aoliveira@sptech.school>
2022-09-28 15:04:28 -0300

    novo projeto

---
+++ lista06-01221161-matheus-oliveira/src/main/java/com/mycompany/lista06/matheus/oliveira/ListaFrutas.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.lista06.matheus.oliveira;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author zagreu
+ */
+public class ListaFrutas {
+    
+    public List<String> getFrutas(){
+      List<String> frutas = new ArrayList();
+    
+        frutas.add("Banana");
+        frutas.add("Maçã");
+        frutas.add("Pera");
+        frutas.add("Melancia");
+        frutas.add("Laranja");
+        frutas.add("Limão");
+        
+        return frutas;
+    }
+    
+}
